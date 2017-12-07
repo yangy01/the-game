2491768c1f8e31496c419673792a8da521514aed
Tyler Fischer <fischt77@uwosh.edu>
2017-12-06 23:19:38 -0600

    update

--- Villain.js
+++ Villain.js
@@ -120,12 +120,12 @@ Villain.prototype.init = function() {
 };
 
 Villain.prototype.show = function() {
-
+     gl.enable(gl.DEPTH_TEST);
     g_matrixStack.push(modelViewMatrix);
     //this.rotate = this.rotate + 0.5;
     modelViewMatrix = mult(modelViewMatrix, translate(this.x, 0.0, this.z));
     modelViewMatrix = mult(modelViewMatrix, scalem(20.0,20.0,20.0));
-    //modelViewMatrix = mult(modelViewMatrix, rotateY(this.rotate));
+    modelViewMatrix = mult(modelViewMatrix, rotateY(this.rotate));
 
     gl.bindBuffer( gl.ARRAY_BUFFER, this.vBuffer );
     this.vPosition = gl.getAttribLocation( program, "vPosition" );
@@ -152,11 +152,10 @@ Villain.prototype.show = function() {
     gl.enableVertexAttribArray(this.vTexCoord);
 
     gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.iBuffer );
-
-    gl.enable(gl.CULL_FACE);	
-    //gl.cullFace(gl.BACK);
-    gl.cullFace(gl.FRONT);
-    gl.cullFace(gl.BACK);
+    // gl.enable(gl.CULL_FACE);
+    // console.log(gl.FRONT);  
+    // console.log(gl.BACK);	
+    // gl.cullFace();
     gl.uniform1i(gl.getUniformLocation(program, "texture_flag"),
           1);
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
--- maze.js
+++ maze.js
@@ -11,11 +11,10 @@ function Maze(program, x, y, z, degrees, bounding_cir_rad)  {
     
         this.vBuffer = null;
         this.nBuffer = null;
-        this.iBuffer = null;
+        // this.iBuffer = null;
         this.vPosition = null;
         this.vNormal = null;
     };
-    
     Maze.prototype = Object.create(GameObject.prototype);
     
     Maze.prototype.init = function() {
@@ -30,6 +29,22 @@ function Maze(program, x, y, z, degrees, bounding_cir_rad)  {
         this.iBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.mazeIndices), gl.STATIC_DRAW);
+
+        /*var image0 = new Image();
+        image0.crossOrigin = "anonymous";
+        image0.src = "bingley128.png";
+        image0.onload = function() { 
+        var texture0 = gl.createTexture();
+        gl.activeTexture( gl.TEXTURE0);
+        gl.bindTexture( gl.TEXTURE_2D, texture0 );
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+                  gl.UNSIGNED_BYTE, image0);
+        gl.generateMipmap( gl.TEXTURE_2D );
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, 
+                  gl.NEAREST_MIPMAP_LINEAR );
+        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+        };*/
         
     };
     
@@ -56,12 +71,12 @@ function Maze(program, x, y, z, degrees, bounding_cir_rad)  {
         gl.enableVertexAttribArray( this.vNormal );
     
         gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.iBuffer );
-        gl.enable(gl.CULL_FACE);	
-        gl.cullFace(gl.BACK);
 
+        //gl.uniform1i(gl.getUniformLocation(program, "texture_flag"),
+        //1);
         var ambientProduct = mult(la0, red);
         var diffuseProduct = mult(ld0, red);
-        var specularProduct = mult(ls0, red);
+        var specularProduct = mult(ls0, red);    
         
         gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),
                   flatten(ambientProduct));
@@ -73,17 +88,38 @@ function Maze(program, x, y, z, degrees, bounding_cir_rad)  {
                   flatten(lp0) );
         gl.uniform1f(gl.getUniformLocation(program, "shininess"),
                  me);
-        gl.disable(gl.CULL_FACE);
+
                  
+        
+        /*gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+        gl.drawElements( gl.TRIANGLES, this.mazeIndices.length, gl.UNSIGNED_SHORT, 0 ); 
+        gl.uniform1i(gl.getUniformLocation(program, "texture"), 0); 
+        gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0 );  
+        gl.uniform1i(gl.getUniformLocation(program, "texture"), 1); 
+        gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 12 ); 
+        gl.uniform1i(gl.getUniformLocation(program, "texture"), 1);   // See from top
+        gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 24 ); 
+        gl.uniform1i(gl.getUniformLocation(program, "texture"), 1);   // See on right
+        gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 36 ); 
+        gl.uniform1i(gl.getUniformLocation(program, "texture"), 0); 
+        gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 48 );
+        gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);   // See on left
+        gl.drawElements( gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 60 );*/
+        
         gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
         gl.drawElements( gl.TRIANGLES, this.mazeIndices.length, gl.UNSIGNED_SHORT, 0 ); 
         
 
         modelViewMatrix = g_matrixStack.pop();
+        //gl.uniform1i(gl.getUniformLocation(program, "texture_flag"),
+        //0);
         // IMPORTANT: Disable current vertex attribute arrays so those in
         // a different object can be activated
         gl.disableVertexAttribArray(this.vPosition);
         gl.disableVertexAttribArray(this.vNormal);
+        
+
+
     
     };
     
